f83ac1135045447c76bf2e7c4b55ece34459d191
Morty <30285840+4bade3@users.noreply.github.com>
2024-01-30 11:08:28 +0800

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -2,13 +2,12 @@
 #
 
 import os
-
 from conan import ConanFile
 from conan.tools.cmake import CMake
 
 
 class FBX2glTFConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "os.subsystem", "os.subsystem.ios_version"
     requires = (
         ("boost/1.78.0"),
         ("libiconv/1.17"),
@@ -26,6 +25,10 @@ class FBX2glTFConan(ConanFile):
             raise Exception(
                 "Rerun 'conan install' with argument: '-s compiler.libcxx=libstdc++11'"
             )
+        if self.settings.os.subsystem == "catalyst":
+            if not hasattr(self.settings, "os.subsystem.ios_version"):
+                self.output.warn("os.subsystem.ios_version is not defined. Assuming a default value.")
+                self.settings.os.subsystem.ios_version = "13.0"
 
     def build(self):
         cmake = CMake(self)
